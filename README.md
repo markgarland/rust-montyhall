9ed325f6962066ad82c21b2f47f2e14b8af42ea1
Mark Garland <github@markgarland.co.uk>
2026-01-09 17:23:31 +0000

    Working example.  Proves it is best to swap

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # rust-montyhall
+
+Example run...
+```
+[Car, Goat, Goat]
+Player selects box 2
+Goats are in boxes [1, 2]
+Monty's options are [1] and he chooses 1
+Player's options are [0]
+Player wins with no swap false, player wins with swap true
+Player wins with no swap total = 0, player wins with swap total = 1
+```
+
+On a billion runs...
+```
+Player wins with no swap total = 333338674, player wins with swap total = 666661326
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use rand::prelude::*;
+use crate::Item::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut rng = rand::rng();
+
+    let mut no_swap_total = 0;
+    let mut swap_total = 0;
+    let number_of_runs = 1;
+    for _ in 1..= number_of_runs {
+        run_game(&mut no_swap_total, &mut swap_total, &mut rng);
+    };
+    println!("Player wins with no swap total = {:?}, player wins with swap total = {:?}", no_swap_total, swap_total);
+
+}
+
+fn run_game (no_swap_total: &mut i32, swap_total: &mut i32, rng: &mut ThreadRng) {
+    let mut boxes = vec![Car, Goat, Goat];
+    boxes.shuffle(rng);
+    println!("{:?}", boxes);
+
+    let players_choice: i8 = rng.random_range(0..=2);
+    println!("Player selects box {}", players_choice);
+
+    let goats: Vec<i8> = boxes
+        .iter()
+        .enumerate()
+        .filter_map(|(index, item)| {
+            if *item == Goat {
+                Some(index as i8)
+            } else {
+                None
+            }
+        }
+        )
+        .collect();
+    println!("Goats are in boxes {:?}", goats);
+
+    let montys_options: Vec<i8> = goats.into_iter().filter(|x| *x != players_choice).collect();
+    let montys_choice = *montys_options.choose(rng).unwrap();
+    println!("Monty's options are {:?} and he chooses {:?}", montys_options, montys_choice);
+
+    let player_options: Vec<i8> = (0..=2).filter(|x| {
+        *x != montys_choice && *x != players_choice
+    }).collect();
+    let player_option: i8 = *player_options.get(0).unwrap();
+    println!("Player's options are {:?}", player_options);
+
+    let player_wins_with_no_swap = boxes[players_choice as usize] == Car;
+    let player_wins_with_swap = boxes[player_option as usize] == Car;
+    println!("Player wins with no swap {:?}, player wins with swap {:?}", player_wins_with_no_swap, player_wins_with_swap);
+
+    if player_wins_with_no_swap {
+        *no_swap_total = *no_swap_total + 1;
+    } else if player_wins_with_swap {
+        *swap_total = *swap_total + 1;
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Item {
+    Goat,
+    Car
 }
